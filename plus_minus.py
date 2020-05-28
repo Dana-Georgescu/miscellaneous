51b7759e8f510ea9948c79e59037155427602a6f
Dana Georgescu <danageorgescu311@gmail.com>
2020-05-28 20:17:09 +0300

    Plus minus challenge

---
+++ plus_minus.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+"Challenge https://www.hackerrank.com/challenges/plus-minus/problem"
+
+def plusMinus():
+    negative = positive = zero = 0
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    for nb in arr:
+        if nb < 0:
+            negative += 1
+        elif nb > 0:
+            positive += 1
+        else:
+            zero += 1
+    negative_ratio = round((negative / n), 6)
+    positive_ratio = round(float(positive / n), 6)
+    zero_ratio = round(zero / n, 6)
+    return "{0:.6f}".format(positive_ratio) +'\n' + "{0:.6f}".format(negative_ratio) + '\n' + "{0:.6f}".format(zero_ratio)
+    
+    
+        
+            
+
+
+
+print(plusMinus())
+
